60e74255c115c1451f7aa04a319df7a955553eb0
martin prado <martinprado1000@gmail.com>
2025-07-09 08:28:26 -0300

    first commit

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,4 @@
 interface ImportMetaEnv {
-  readonly VITE_TEMPLATE_IMAGE_URL: string; // ← Asegúrate de que coincida con tu variable en .env
+  readonly VITE_TEMPLATE_IMAGE_URL: string;
   readonly VITE_URL_BACK: string;
-  // Otras variables de entorno de Vite...
 }
